7df1494d0cbfedfd9bdda8173e728808deea3938
VIVEK-THAKUR626 <vistaar4324@gmail.com>
2026-02-20 21:51:21 +0530

    create the logic of game loop

--- main.java
+++ main.java
@@ -1,7 +1,44 @@
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
 
 public class main {
+	static void gameLoop(Scanner sc, String chosenWord, String userWord){
+		for(int i=0; i<chosenWord.length(); i++){
+			System.out.println("\nEnter your guess number "+(i+1));
+			userWord = sc.next();
+
+			String greenBackdrop = "\033[30;102m";
+			String yellowBackdrop = "\033[30;103m";
+			String whiteBackdrop = "\033[30;107m";
+			String resetColor =  "\033[0m";
+
+			int gotRight = 0;
+			for(int j=0; j<chosenWord.length(); j++){
+				if(chosenWord.charAt(j) == userWord.charAt(j)){
+					System.out.print(greenBackdrop+" "+Character.toUpperCase(userWord.charAt(j))+" "+resetColor);
+					gotRight++;
+				}else{
+					int found = 0;
+					for(int k=0; k<chosenWord.length(); k++){
+						if(userWord.charAt(j) == chosenWord.charAt(k)){
+							found = 1;
+							break;
+						}
+					}
+					if(found == 1){
+						System.out.print(yellowBackdrop+" "+Character.toUpperCase(userWord.charAt(j))+" "+resetColor);
+					}else{
+						System.out.print(whiteBackdrop+" "+Character.toUpperCase(userWord.charAt(j))+" "+resetColor);
+					}
+				}
+				if(gotRight == chosenWord.length()){
+					System.out.println("\nYOU GUESSED THE WORD CORRECTLY: "+chosenWord);
+					return;
+				}
+			}
+		}
+		System.out.println("\nYOU ARE OUT OF ATTEMPTS, THE WORD WAS: "+chosenWord);
+	}
 	public static void main(String[] args){
 		try{
 			Reader r = new FileReader("1000-most-common-words.txt");
@@ -20,9 +57,11 @@ public class main {
 			r.close();
 			int randomWordIndex = (int)(Math.random()*(wordList.size()-0)) + 0;
 			String chosenWord = wordList.get(randomWordIndex);
-			System.out.println(wordList.size());
-			System.out.println(chosenWord);
-			char[] userWord = new char[chosenWord.length()];
+			String userWord = "";
+			Scanner sc = new Scanner(System.in);
+
+			System.out.println("The word is "+chosenWord.length()+" letters long");
+			gameLoop(sc, chosenWord, userWord);
 		}
 		catch(Exception error){
 			System.out.println("An error occurred: "+error.getMessage());
